b6cbe04cb3eef9571ad015d35e52dad46e32c5db
Jonathan Pelletier <jpelletier@onica.com>
2020-06-16 16:08:12 -0400

    adding new stuff

--- variables.tf
+++ variables.tf
@@ -1,3 +1,3 @@
 variable "secret_arn" {
-    description = "Arn of the resource specifying the secret to be accessible"
+    description = "Arn of the resource specifying the secret to be accessible ajjjs"
 }
